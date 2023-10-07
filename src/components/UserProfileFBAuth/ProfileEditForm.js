b2e60c5e13fbf684046abf090e200d24a05f8e27
Michael Carroll <mcarroll138@gmail.com>
2023-10-07 15:07:59 -0700

    update user profile creation with user auth display name update

--- src/components/UserProfileFBAuth/ProfileEditForm.js
+++ src/components/UserProfileFBAuth/ProfileEditForm.js
@@ -1,18 +1,92 @@
-import { updateProfile } from "firebase/auth";
-import React from "react";
+// import { updateProfile } from "firebase/auth";
+// import React from "react";
+
+// export default function ProfileEditForm({
+//   auth,
+//   newDisplayName,
+//   setNewDisplayName,
+//   setEditProfile,
+// }) {
+//   const ContainerStyles = {
+//     alignItems: "center",
+//     backgroundColor: "pink",
+//     padding: "10px",
+//   };
+
+//   const formStyles = {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//   };
+
+//   const inputStyles = {
+//     margin: "4px",
+//     padding: "4px",
+//     border: "1px solid #ccc",
+//     borderRadius: "4px",
+//     fontSize: "12px",
+//   };
+
+//   const buttonStyles = {
+//     width: 180,
+//     height: 20,
+//     paddingLeft: 24,
+//     paddingRight: 24,
+//     paddingTop: 20,
+//     paddingBottom: 20,
+//     background: "black",
+//     boxShadow: "6px 6px 6px #E3A9FF",
+//     border: "2px #E3A9FF solid",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     gap: 10,
+//     color: "white",
+//   };
+//   function handleProfileEditFormSubmission(event) {
+//     event.preventDefault();
+//     updateProfile(auth.currentUser, { displayName: newDisplayName })
+//       .then(() => {
+//         setEditProfile(false);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleProfileEditFormSubmission} style={formStyles}>
+//         <h2>Update Name</h2>
+//         <input
+//           style={inputStyles}
+//           type="text"
+//           name="displayName"
+//           defaultValue={auth.currentUser.displayName}
+//           onChange={(e) => setNewDisplayName(e.target.value)}
+//         />
+
+//         <button type="submit" style={buttonStyles}>
+//           Update
+//         </button>
+//         <button style={buttonStyles} onClick={() => setEditProfile(false)}>
+//           Cancel
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
+
+import React, { useEffect } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function ProfileEditForm({
-  auth,
   newDisplayName,
   setNewDisplayName,
   setEditProfile,
 }) {
-  const ContainerStyles = {
-    alignItems: "center",
-    backgroundColor: "pink",
-    padding: "10px",
-  };
-
   const formStyles = {
     display: "flex",
     flexDirection: "column",
@@ -43,29 +117,55 @@ export default function ProfileEditForm({
     gap: 10,
     color: "white",
   };
-  function handleProfileEditFormSubmission(event) {
+
+  useEffect(() => {
+    const auth = getAuth();
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.displayName);
+        console.log(user.email);
+      } else {
+        console.log("error");
+      }
+    });
+  }, []);
+
+  const handleProfileEditFormSubmission = async (event) => {
     event.preventDefault();
-    updateProfile(auth.currentUser, { displayName: newDisplayName })
-      .then(() => {
-        setEditProfile(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+
+    const auth = getAuth();
+    try {
+      await updateProfile(auth.currentUser, { displayName: newDisplayName });
+
+      const newProfileData = {
+        uid: auth.currentUser.uid,
+        userProfile: auth.currentUser.email,
+        displayName: newDisplayName,
+        profilePhoto: auth.currentUser.photoURL,
+        friends: [],
+      };
+
+      const docRef = await addDoc(collection(db, "profiles"), newProfileData);
+      console.log("Document written with ID:", docRef.id);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setEditProfile(false);
+  };
 
   return (
     <>
       <form onSubmit={handleProfileEditFormSubmission} style={formStyles}>
-        <h2>Update Name</h2>
+        <h2>Complete Registration</h2>
         <input
           style={inputStyles}
           type="text"
           name="displayName"
-          defaultValue={auth.currentUser.displayName}
+          defaultValue={newDisplayName}
           onChange={(e) => setNewDisplayName(e.target.value)}
+          placeholder="Display Name"
         />
-
         <button type="submit" style={buttonStyles}>
           Update
         </button>
